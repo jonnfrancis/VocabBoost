1603232d71f941188a1ef58130dc3f92cda4462e
John Francis <johnmosima7@gmail.com>
2023-08-13 11:21:52 +0300

    updated bottom navbar

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -2,16 +2,16 @@ import back from '../assets/bx-rewind.svg.svg'
 import front from '../assets/bx-fast-forward.svg.svg'
 import home from '../assets/bxs-store-alt.svg.svg'
 
-const BottomNavBar = ({ onBack, onHome, onNext }) => {
+const BottomNavBar = () => {
     return (
         <div className="bottom-navbar">
-            <div className="nav-button" onClick={onBack}>
+            <div className="nav-button">
                 <img src={back} alt="rewind" />
             </div>
-            <div className="nav-button" id="home" onClick={onHome}>
+            <div className="nav-button" id="home">
                 <img src={home} alt="rewind" />
             </div>
-            <div className="nav-button" onClick={onNext}>
+            <div className="nav-button">
                 <img src={front} alt="rewind" />
             </div>
         </div>
