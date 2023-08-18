c52be1ad9a0dd79c1a0d9b302314f6e6c7701cf2
John Francis <johnmosima7@gmail.com>
2023-08-18 19:36:42 +0300

    updated GA intergration

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Radar from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    Radar({
-      // Google Analytics tag injection
-      gaMeasurementId: 'G-XH90TFFRFZ'
-    })
+    react()
   ],
   base:"/VocabBoost/"
 })
