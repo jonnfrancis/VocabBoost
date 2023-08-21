36bb5f1d0fb1884a1ac739f715569510c080e636
John Francis <johnmosima7@gmail.com>
2023-08-21 21:36:22 +0300

    Shuffled images

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useState, useEffect } from 'react'
 import Observer from 'gsap/Observer';
 import SplitType from 'split-type'
-import img1 from './assets/1.jpg';
-import img2 from './assets/1.png';
+import img1 from './assets/1.png';
+import img2 from './assets/2.png';
 import img3 from './assets/3.png';
 import img4 from './assets/4.png';
 import img5 from './assets/5.png';
